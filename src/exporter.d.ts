f08c5d99b8135006dccdd2a933a50efab0c73d40
Clayton Grassick <clayton@claytronics.org>
2020-11-10 09:23:50 -0500

    Added TS defs

---
+++ src/exporter.d.ts
@@ -0,0 +1,6 @@
+/** 
+ * Export file to XLSX
+ * @param dataFile e.g. "localizations.json"
+ * @param xlsxFile path of file to export
+ */
+export default function exportLocalizationFileToXlsx(dataFile: string, xlsxFile: string): void
