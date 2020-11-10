52c84e670447739e4b8052a733c9e75e45b430f1
Clayton Grassick <clayton@claytronics.org>
2020-11-10 09:23:50 -0500

    Added TS defs

---
+++ src/extractor/index.d.ts
@@ -0,0 +1,21 @@
+import { LocalizerData } from "../.."
+
+/**
+ * Updates a localization file on disk
+ * rootDirs: directories to extract from. Can also include simple files
+ * dataFile: e.g. "localizations.json"
+ * options: 
+ *   extraStrings: include extra strings that are not in the root file
+ * @param callback Called when complete
+ */
+export function updateLocalizationFile(rootDirs: string[], dataFile: string, options: { extraStrings?: string[] }, callback: () => void): void
+
+/**
+ * Updates localization data in place
+ * rootDirs: directories to extract from. Can also include simple files
+ * dataFile: e.g. "localizations.json"
+ * options: 
+ *   extraStrings: include extra strings that are not in the root file
+ * @param callback Called when complete
+ */
+export function updateLocalizations(rootDirs: string[], data: LocalizerData, options: { extraStrings?: string[] }, callback: () => void): void
