b125d3dd5616ae19177d4e85cee511394f275c24
decaffeinate <clayton@claytronics.org>
2021-09-17 14:08:49 -0400

    decaffeinate: Convert export.coffee and 13 other files to JS

--- test/utilsTests.js
+++ test/utilsTests.js
@@ -1,12 +1,11 @@
-_ = require 'lodash'
-assert = require("chai").assert
+import _ from 'lodash';
+import { assert } from "chai";
+import utils from '../src/utils';
 
-utils = require '../src/utils'
-
-describe "Localizer", ->
-  describe "extractLocalizedStrings", ->
-    it "gets all strings", ->
-      obj = {
+describe("Localizer", function() {
+  describe("extractLocalizedStrings", function() {
+    it("gets all strings", function() {
+      const obj = {
         a: [
           { 
             b: { _base: "en", en: "hello" }
@@ -14,16 +13,17 @@ describe "Localizer", ->
           { 
             c: { _base: "es", en: "hello2" }
           }
-        ]
-        d: "test"
+        ],
+        d: "test",
         e: null
-      }
-      strs = utils.extractLocalizedStrings(obj)
+      };
+      const strs = utils.extractLocalizedStrings(obj);
 
-      assert.deepEqual strs, [{ _base: "en", en: "hello" }, { _base: "es", en: "hello2" }]
+      return assert.deepEqual(strs, [{ _base: "en", en: "hello" }, { _base: "es", en: "hello2" }]);
+  });
 
-    it "gets localizedStrings strings", ->
-      obj = {
+    return it("gets localizedStrings strings", function() {
+      const obj = {
         localizedStrings: [
           { 
             b: { _base: "en", en: "hello" }
@@ -31,208 +31,228 @@ describe "Localizer", ->
           { 
             c: { _base: "es", en: "hello2" }
           }
-        ]
-        d: "test"
+        ],
+        d: "test",
         e: null
-      }
-      strs = utils.extractLocalizedStrings(obj)
+      };
+      const strs = utils.extractLocalizedStrings(obj);
 
-      assert.deepEqual strs, [{ _base: "en", en: "hello" }, { _base: "es", en: "hello2" }]
+      return assert.deepEqual(strs, [{ _base: "en", en: "hello" }, { _base: "es", en: "hello2" }]);
+  });
+});
 
-  describe "dedupLocalizedStrings", ->
-    it "removes duplicates by base + string", ->
-      strs = [
-        { _base: "en", en: "hello", es: "esp1" }
-        { _base: "es", en: "hello", es: "hello" }  # Different base language
-        { _base: "en", en: "hello2" }
-        { _base: "en", en: "hello", es: "esp2" }
-      ]
+  describe("dedupLocalizedStrings", () => it("removes duplicates by base + string", function() {
+    const strs = [
+      { _base: "en", en: "hello", es: "esp1" },
+      { _base: "es", en: "hello", es: "hello" },  // Different base language
+      { _base: "en", en: "hello2" },
+      { _base: "en", en: "hello", es: "esp2" }
+    ];
 
-      strs2 = utils.dedupLocalizedStrings(strs)
-      assert.deepEqual strs2, [
-        { _base: "en", en: "hello", es: "esp1" }
-        { _base: "es", en: "hello", es: "hello" }  # Different base language
-        { _base: "en", en: "hello2" }
-      ]
+    const strs2 = utils.dedupLocalizedStrings(strs);
+    assert.deepEqual(strs2, [
+      { _base: "en", en: "hello", es: "esp1" },
+      { _base: "es", en: "hello", es: "hello" },  // Different base language
+      { _base: "en", en: "hello2" }
+    ]);
 
-      # Should keep orig object
-      assert strs2[0] == strs[0]
+    // Should keep orig object
+    return assert(strs2[0] === strs[0]);
+}));
 
-  describe "changeBaseLocale", ->
-    it "changes base, preserving other strings but overwriting with base", ->
-      strs = [
-        { _base: "en", en: "hello", es: "esp1" }
-        { _base: "en", en: "hello", fr: "fr1", es: "esp1" }
-        { _base: "es", en: "hello", fr: "fr1", es: "esp1" }
-        { _base: "fr", fr: "fr2", es: "esp1" }
-        { _base: "es", fr: "fr2", es: "esp1" }
-      ]
+  describe("changeBaseLocale", () => it("changes base, preserving other strings but overwriting with base", function() {
+    const strs = [
+      { _base: "en", en: "hello", es: "esp1" },
+      { _base: "en", en: "hello", fr: "fr1", es: "esp1" },
+      { _base: "es", en: "hello", fr: "fr1", es: "esp1" },
+      { _base: "fr", fr: "fr2", es: "esp1" },
+      { _base: "es", fr: "fr2", es: "esp1" }
+    ];
 
-      utils.changeBaseLocale(strs, "en", "fr")
-      assert.deepEqual strs, [
-        { _base: "fr", fr: "hello", es: "esp1" }
-        { _base: "fr", fr: "hello", es: "esp1" }
-        { _base: "fr", fr: "hello", es: "esp1" }
-        { _base: "fr", fr: "fr2", es: "esp1" }
-        { _base: "fr", fr: "esp1" }
-      ], JSON.stringify(strs, null, 2)
+    utils.changeBaseLocale(strs, "en", "fr");
+    return assert.deepEqual(strs, [
+      { _base: "fr", fr: "hello", es: "esp1" },
+      { _base: "fr", fr: "hello", es: "esp1" },
+      { _base: "fr", fr: "hello", es: "esp1" },
+      { _base: "fr", fr: "fr2", es: "esp1" },
+      { _base: "fr", fr: "esp1" }
+    ], JSON.stringify(strs, null, 2));
+  }));
 
-  describe "xlsx export/import", ->
-    beforeEach ->
-      @localizer = utils
-      @locales = [{code: "en", name: "English"}, {code: "es", name: "Espanol"}]
-      @roundtrip = (strs) ->
-        xlsxFile = @localizer.exportXlsx(@locales, strs)
-        return @localizer.importXlsx(@locales, xlsxFile)
+  describe("xlsx export/import", function() {
+    beforeEach(function() {
+      this.localizer = utils;
+      this.locales = [{code: "en", name: "English"}, {code: "es", name: "Espanol"}];
+      return this.roundtrip = function(strs) {
+        const xlsxFile = this.localizer.exportXlsx(this.locales, strs);
+        return this.localizer.importXlsx(this.locales, xlsxFile);
+      };
+    });
 
-    it "roundtrips strings", ->
-      input = [
-        { _base: "en", en: "hello", es: "hola" }
+    it("roundtrips strings", function() {
+      const input = [
+        { _base: "en", en: "hello", es: "hola" },
         { _base: "es", en: "bye", es: "ciao" }
-      ]
+      ];
 
-      output = @roundtrip input
-      assert.deepEqual input, output
+      const output = this.roundtrip(input);
+      return assert.deepEqual(input, output);
+    });
 
-    it "preserves special characters", ->
-      input = [
-        { _base: "en", en: "<>`'\"!@#$%^&*()_+", es: "hola" }
+    it("preserves special characters", function() {
+      const input = [
+        { _base: "en", en: "<>`'\"!@#$%^&*()_+", es: "hola" },
         { _base: "es", en: "bye", es: "ciao" }
-      ]
+      ];
 
-      output = @roundtrip input
-      assert.deepEqual input, output
+      const output = this.roundtrip(input);
+      return assert.deepEqual(input, output);
+    });
 
-    it "preserves unicode characters", ->      
-      input = [
-        { _base: "en", en: "éא", es: "hola" }
+    it("preserves unicode characters", function() {      
+      const input = [
+        { _base: "en", en: "éא", es: "hola" },
         { _base: "es", en: "bye ", es: "ciao" }
-      ]
+      ];
 
-      output = @roundtrip input
-      assert.deepEqual input, output
+      const output = this.roundtrip(input);
+      return assert.deepEqual(input, output);
+    });
 
-    it "preserves enter", ->
-      input = [
-        { _base: "en", en: "hello\nhello2", es: "hola" }
+    it("preserves enter", function() {
+      const input = [
+        { _base: "en", en: "hello\nhello2", es: "hola" },
         { _base: "es", en: "bye", es: "ciao" }
-      ]
+      ];
 
-      output = @roundtrip input
-      assert.deepEqual input, output
+      const output = this.roundtrip(input);
+      return assert.deepEqual(input, output);
+    });
 
-    it "preserves numbers as strings", ->
-      input = [
-        { _base: "en", en: "2", es: "3" }
+    it("preserves numbers as strings", function() {
+      const input = [
+        { _base: "en", en: "2", es: "3" },
         { _base: "es", en: "bye", es: "ciao" }
-      ]
+      ];
 
-      output = @roundtrip input
-      assert.deepEqual input, output
+      const output = this.roundtrip(input);
+      return assert.deepEqual(input, output);
+    });
 
-    it "returns empty strings", ->
-      input = [
+    return it("returns empty strings", function() {
+      const input = [
         { _base: "en", en: "2" }
-      ]
+      ];
 
-      output = @roundtrip input
-      assert.deepEqual output, [ { _base: "en", en: "2", es: "" }]
+      const output = this.roundtrip(input);
+      return assert.deepEqual(output, [ { _base: "en", en: "2", es: "" }]);
+  });
+});
 
-  describe "updateLocalizedStrings", ->
-    beforeEach ->
-      @localizer = utils
+  return describe("updateLocalizedStrings", function() {
+    beforeEach(function() {
+      return this.localizer = utils;
+    });
 
-    it "updates based on base + string", ->
-      strs = [
-        { _base: "en", en: "hello", es: "hola1" }
-        { _base: "en", en: "hello", es: "hola2" }
+    it("updates based on base + string", function() {
+      const strs = [
+        { _base: "en", en: "hello", es: "hola1" },
+        { _base: "en", en: "hello", es: "hola2" },
         { _base: "es", en: "bye", es: "ciao" }
-      ]
+      ];
 
-      updates = [
-        { _base: "en", en: "hello", es: "hola3" }
-        { _base: "en", en: "bye", es: "ciao2" }  # Should not apply since wrong base
-      ]
+      const updates = [
+        { _base: "en", en: "hello", es: "hola3" },
+        { _base: "en", en: "bye", es: "ciao2" }  // Should not apply since wrong base
+      ];
 
-      @localizer.updateLocalizedStrings(strs, updates)
+      this.localizer.updateLocalizedStrings(strs, updates);
 
-      assert.deepEqual strs, [
-        { _base: "en", en: "hello", es: "hola3" }
-        { _base: "en", en: "hello", es: "hola3" }
+      return assert.deepEqual(strs, [
+        { _base: "en", en: "hello", es: "hola3" },
+        { _base: "en", en: "hello", es: "hola3" },
         { _base: "es", en: "bye", es: "ciao" }
-      ]
+      ]);
+  });
 
-    it "updates using trimmed string", ->
-      strs = [
+    it("updates using trimmed string", function() {
+      const strs = [
         { _base: "en", en: "hello ", es: "hola1" }
-      ]
+      ];
 
-      updates = [
+      const updates = [
         { _base: "en", en: "hello", es: "hola3" }
-      ]
+      ];
 
-      @localizer.updateLocalizedStrings(strs, updates)
+      this.localizer.updateLocalizedStrings(strs, updates);
 
-      assert.deepEqual strs, [
+      return assert.deepEqual(strs, [
         { _base: "en", en: "hello ", es: "hola3" }
-      ], JSON.stringify(strs)
+      ], JSON.stringify(strs));
+    });
 
-    it "updates using CR/LF normalized string", ->
-      strs = [
+    it("updates using CR/LF normalized string", function() {
+      const strs = [
         { _base: "en", en: "hello\nthere", es: "hola1" }
-      ]
+      ];
 
-      updates = [
+      const updates = [
         { _base: "en", en: "hello\r\nthere", es: "hola3" }
-      ]
+      ];
 
-      @localizer.updateLocalizedStrings(strs, updates)
+      this.localizer.updateLocalizedStrings(strs, updates);
 
-      assert.deepEqual strs, [
+      return assert.deepEqual(strs, [
         { _base: "en", en: "hello\nthere", es: "hola3" }
-      ], JSON.stringify(strs)
+      ], JSON.stringify(strs));
+    });
 
-    it "leaves unknown strings untouched", ->
-      strs = [
+    it("leaves unknown strings untouched", function() {
+      const strs = [
         { _base: "en", en: "other", es: "otra" }
-      ]
+      ];
 
-      updates = [
-        { _base: "en", en: "hello", es: "hola3" }
-        { _base: "en", en: "bye", es: "ciao2" }  # Should not apply since wrong base
-      ]
+      const updates = [
+        { _base: "en", en: "hello", es: "hola3" },
+        { _base: "en", en: "bye", es: "ciao2" }  // Should not apply since wrong base
+      ];
 
-      @localizer.updateLocalizedStrings(strs, updates)
+      this.localizer.updateLocalizedStrings(strs, updates);
 
-      assert.deepEqual strs, [
+      return assert.deepEqual(strs, [
         { _base: "en", en: "other", es: "otra" }
-      ]
+      ]);
+  });
 
-    it "removes empty strings", ->
-      strs = [
+    it("removes empty strings", function() {
+      const strs = [
         { _base: "en", en: "other", es: "otra" }
-      ]
+      ];
 
-      updates = [
-        { _base: "en", en: "other", es: "" } # Should remove es
-      ]
+      const updates = [
+        { _base: "en", en: "other", es: "" } // Should remove es
+      ];
 
-      @localizer.updateLocalizedStrings(strs, updates)
+      this.localizer.updateLocalizedStrings(strs, updates);
 
-      assert.deepEqual strs, [ { _base: "en", en: "other" } ], JSON.stringify(strs)
+      return assert.deepEqual(strs, [ { _base: "en", en: "other" } ], JSON.stringify(strs));
+    });
 
-    it "leaves unknown languages untouched", ->
-      strs = [
+    return it("leaves unknown languages untouched", function() {
+      const strs = [
         { _base: "en", en: "hello", es: "hola1", fr: "bonjour" }
-      ]
+      ];
 
-      updates = [
+      const updates = [
         { _base: "en", en: "hello", es: "hola3" }
-      ]
+      ];
 
-      @localizer.updateLocalizedStrings(strs, updates)
+      this.localizer.updateLocalizedStrings(strs, updates);
 
-      assert.deepEqual strs, [
+      return assert.deepEqual(strs, [
         { _base: "en", en: "hello", es: "hola3", fr: "bonjour" }
-      ]
+      ]);
+  });
+});
+});
 
