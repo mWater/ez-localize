9ab956aed7ce862bcf9acca0f20bae2a571ed379
Clayton Grassick <clayton@claytronics.org>
2022-11-09 09:34:28 -0500

    Update localize string

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ export interface LocalizerData {
 export { default as Localizer } from './Localizer'
 
 /** Function to localize a string. Usually exposed as "T" */
-export type LocalizeString = ((str: string, ...args: any[]) => string) & ({ locale: string, localizer: Localizer })
+export type LocalizeString = ((str: string[] | string | null | undefined, ...args: any[]) => string) & ({ locale: string, localizer: Localizer })
 
 // Create default localizer
 var defaultLocalizer = new Localizer({ locales: [{ code: "en", name: "English" }], strings: [] })
