9e86f956c5cf25c71a02077473020c4425350356
Clayton Grassick <clayton@claytronics.org>
2024-09-30 16:19:20 -0400

    Compile

--- lib/Localizer.d.ts
+++ lib/Localizer.d.ts
@@ -22,14 +22,31 @@ export default class Localizer {
      * Localize a single string of the form "some text {0} more text {1} etc", replacing the
      * parts with the arguments.
      *
-     * Can also replace where the first parameter is an array for ES6 tagged templates
+     * Can also replace where the first parameter is an array for ES6 tagged templates.
+     *
+     * Can also localize to a specified locale with LocalizationRequest.
      */
-    localizeString: (str: TemplateStringsArray | LocalizedString | string | null | undefined, ...args: any[]) => string | null;
+    localizeString: (str: TemplateStringsArray | LocalizedString | LocalizationRequest | string | null | undefined, ...args: any[]) => string | null;
     /**
      * Localizes a plain string without React-style interpretation. Needed for handlebars as it passes extra arguments
      */
-    localizePlainString: (str: any, ...args: any[]) => string;
+    localizePlainString: (str: any, ...args: any[]) => string | null;
+    /**
+     * Localizes a string based on a localization request.
+     */
+    private localizeStringRequest;
     /** Determines if a string is localized */
     isLocalized(str: string): boolean;
     makeGlobal(handlebars: any): void;
+}
+/**
+ * Localization request
+ */
+export interface LocalizationRequest {
+    /** Locale to localize to. e.g. "en" or "fr" */
+    locale: string;
+    /** Text to localize in format of "some text {0} more text {1} etc" */
+    text: string;
+    /** Arguments to substitute into the localized string */
+    args?: any[];
 }
